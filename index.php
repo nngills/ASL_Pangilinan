f9e6a57f98eb3f1d4559ad5f89d0eb78cf58830a
Gillian Pangilinan <Gill@Gillians-MacBook-Pro.local>
2016-01-13 13:21:12 -1000

    Crafting List HTML
    Added the base HTML for the item list
    Added php to loop through the database to output the crafting list

--- index.php
+++ index.php
@@ -7,8 +7,32 @@
 </head>
 
 <body>
-	Say pal, you don't look so good. You better find something to eat before night comes!
-    <p>If this is red then the CSS works.</p>
+	<h1>Don't Starve Items</h1>
+    
+    <?php
+		//SETUP username, password, and establish PDO and DSN connection to database
+    	$user='root';
+		$pass="root";
+		$dbh = new PDO('mysql:host=localhost;dbname=DontStarveItems;port=8889', $user, $pass);
+		
+		//get Craftable TabNames
+		$stmt = $dbh->prepare('
+			select tabName from tabs
+			order by id;
+		');
+		$stmt->execute();
+		$tabResult = $stmt->fetchall(PDO::FETCH_ASSOC);	
+		
+		//get craftable items and tabs
+		$stmt = $dbh->prepare('
+			select items.itemsName, tabs.tabName from craftable_items
+			join items on items.id = craftable_items.itemId
+			join tabs on tabs.id = craftable_items.tab
+			order by tabs.id;
+		');
+		$stmt->execute();
+		$craft_items_result = $stmt->fetchall(PDO::FETCH_ASSOC);	
+	?>
     
     <main>
     	<article id="top">
@@ -19,29 +43,35 @@
         </section>
     </main>
     
-    <?php
-		//SETUP username, password, and establish PDO and DSN connection to database
-    	$user='root';
-		$pass="root";
-		$dbh = new PDO('mysql:host=localhost;dbname=DontStarveItems;port=8889', $user, $pass);
-		
-		//get Craftable items and Tabs
-		$stmt = $dbh->prepare('
-			select items.itemsName, tabs.tabName 
-			from craftable_items
-			join items
-			on craftable_items.itemId = items.id
-			join tabs
-			on craftable_items.tab = tabs.id
-			order by craftable_items.tab;
-		');
-		$stmt->execute();
-		$result = $stmt->fetchall(PDO::FETCH_ASSOC);	
+    <? 
+	
+	/*
+	<article id="{$row['tabName']}">
+		<h2>{$row['tabName']}</h2>
+		<ul>
+			<li><img></li>
+			<li><img></li>
+		</ul>
+	</article>
+	*/
+	
+	foreach($tabResult as $row){
+		echo "
+			<article id='{$row['tabName']}'>
+				<h2>{$row['tabName']}</h2>
+				<ul>";
+		foreach($craft_items_result as $items_row){
+			if($items_row['tabName'] == $row['tabName']){
+				$itemName = $items_row['itemsName'];
+				echo '<li><img src="images/items/'.$itemName.'.png"></li>';
+			}
+		}
+				
+		echo	"</ul>
+			</article>
+			";
+	}
 	?>
-    
-    <pre>
-    <? print_r($result) ?>
-    </pre>
     
     <!--JAVASCRIPT-->
     <script src="js/jquery-1.12.0.min.js"></script>
