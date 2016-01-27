2410ddb37fb323ef2157bfdedf498b4c04ec9aef
Gillian Pangilinan <Gill@Gillians-MacBook-Pro.local>
2016-01-27 12:27:34 -1000

    Styled the recipe list

--- application/views/pages/recipelist.php
+++ application/views/pages/recipelist.php
@@ -1,13 +1,19 @@
 <section id="recipe_list">
+<p>Recipes that use this item: </p>
 	<?  
 	foreach($ablerecipes as $row){
-		echo $row['itemsName'];
+		echo "<ul>";
+		echo '<li title="'.ucwords(str_replace("_", " ", $row['itemsName'])).'"><a href="index.php?id='.$row['itemsName'].'&version='.$version.'"><img src="assets/images/items/'.$row['itemsName'].'.png"></a></li>';
 		$count = count($row)-2;
+		echo "<ul>";
 		for($x = 0; $x<=$count; $x++){
-			echo $row[$x]['material'];
-			echo $row[$x]['quantity'];
+			echo "<li>";
+			echo '<a href="index.php?id='.$row[$x]['material'].'&version='.$version.'" class="mats" title="'.ucwords(str_replace("_", " ", $row[$x]['material'])).'"><span class="outline"> '.$row[$x]['quantity'].'</span>';
+			echo '<img src="assets/images/items/'.$row[$x]['material'].'.png" ></a>';
+			echo "</li>";
 		}
-		echo "<br/><br/>";
+		echo "</ul>";
+		echo "</ul>";
 	}
 	?>
 </section>
